7ed09fe812230bd46656eb65e53dcfe10f957a03
Dennis <eaa23@students.eaaa.dk>
2024-08-20 22:17:52 +0200

    belgiskFlag

---
+++ src/AP/Tidskompleksitet/AlgoritmeOpgaver/Opgave5_BelgiskFlag.java
@@ -0,0 +1,16 @@
+package AP.Tidskompleksitet.AlgoritmeOpgaver;
+
+public class Opgave5_BelgiskFlag {
+    public static void main(String[] args) {
+
+    }
+
+    public static void belgisk_flag(char[] belgisk_flag){
+        int sTæller = 0;
+        int gTæller = 0;
+        int rTæller = 0;
+        for (int i = 0; i < belgisk_flag.length; i++) {
+
+        }
+    }
+}
