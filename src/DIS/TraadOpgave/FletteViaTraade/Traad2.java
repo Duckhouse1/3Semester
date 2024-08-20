9b09c96ed216b7f807d5b8c74cabe41b49fe4f09
Dennis <eaa23@students.eaaa.dk>
2024-08-20 14:12:03 +0200

    traadFlet ting

--- src/DIS/TraadOpgave/FletteViaTraade/Traad2.java
+++ src/DIS/TraadOpgave/FletteViaTraade/Traad2.java
@@ -1,4 +1,19 @@
 package DIS.TraadOpgave.FletteViaTraade;
 
-public class Traad2 {
+import java.util.List;
+
+public class Traad2 extends Thread{
+    private List<Integer> liste;
+    private FletteSortering sortering;
+
+    public Traad2(List<Integer> liste, FletteSortering sortering) {
+        this.liste = liste;
+        this.sortering = sortering;
+    }
+
+    @Override
+    public void run() {
+        sortering.mergesort(liste,liste.size()/2,liste.size());
+    }
 }
+
