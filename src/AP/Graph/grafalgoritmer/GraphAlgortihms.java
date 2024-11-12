3b8c123506c2dfd7f057cbadd9984ae54a53bb59
Dennis <eaa23ddh@students.eaaa.dk>
2024-11-12 09:07:03 +0100

    graph algoritmer

--- src/AP/Graph/grafalgoritmer/GraphAlgortihms.java
+++ src/AP/Graph/grafalgoritmer/GraphAlgortihms.java
@@ -1,9 +1,6 @@
 package AP.Graph.grafalgoritmer;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class GraphAlgortihms {
     /**
